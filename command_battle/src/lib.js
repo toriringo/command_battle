6f93061c3fdf9492f79dc039228c892d5ba9a181
TORIUMI Mitsugu <toriringo@gmail.com>
2020-08-09 17:39:25 +0900

    modify

--- command_battle/src/lib.js
+++ command_battle/src/lib.js
@@ -623,7 +623,8 @@ function sleep(ms)
 }
 
 // minからmaxまでのランダムな整数を返す
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max)
+{
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
