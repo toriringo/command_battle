9d950d2dc1812af4991d195e06d9a6da748734e3
TORIUMI Mitsugu <toriringo@gmail.com>
2020-08-27 06:15:34 +0900

    modify

--- command_battle_section4/src/game.js
+++ command_battle_section4/src/game.js
@@ -1,8 +1,8 @@
 // キャラクターをインスタンス化する
-let friend1 = new Friend("あれす", 180, 66, 13, 2, 40);                   // 味方
-let friend2 = new Friend("なーしゃ", 110, 16, 12, 3, 40);                 // 味方
-let friend3 = new Friend("だすてん", 140, 43, 11, 1, 40);                 // 味方
-let enemy1 = new Troll("トロル", 250, 38, 20, "../image/troll.png");      // 敵
+let friend1 = new Friend("あれす", 180, 66, 13, 2, 45);                   // 味方
+let friend2 = new Friend("なーしゃ", 110, 16, 12, 3, 45);                 // 味方
+let friend3 = new Friend("だすてん", 140, 43, 11, 1, 45);                 // 味方
+let enemy1 = new Troll("トロル", 270, 38, 20, "../image/troll.png");      // 敵
 let enemy2 = new Dragon("ドラゴン", 380, 68, 6, "../image/dragon.png");   // 敵
 
 // キャラクター配列をつくる
